882622f41c6585ee7a38213f270a50b089ccb996
ShrewdLuni <ander113q@gmail.com>
2024-05-11 20:09:33 +0300

    feat: add project template & add tailwind

---
+++ src/app/features/menu/menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+    selector: 'app-menu',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './menu.component.html',
+})
+export class MenuComponent {}
