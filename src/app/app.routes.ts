963e8ba4c8b02af63596e9a7be195e3095f73509
ShrewdLuni <ander113q@gmail.com>
2024-05-11 20:16:49 +0300

    feat: add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { MenuComponent } from './features/menu/menu.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { ErrorComponent } from './features/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: HomeComponent,},
+    {path: 'menu',component: MenuComponent,},
+    {path: 'dashboard',component: DashboardComponent,},
+    {path: 'error',component: ErrorComponent,},
+    {path: '**',redirectTo: '',},
+];
