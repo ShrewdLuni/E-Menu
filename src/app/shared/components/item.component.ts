08195fef8562a5cf4f22558e8fac351bbcb2ade8
ShrewdLuni <ander113q@gmail.com>
2024-05-20 22:57:35 +0300

    fead: add item component

---
+++ src/app/shared/components/item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+    selector: 'app-item',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './item.component.html',
+})
+export class ItemComponent {}
